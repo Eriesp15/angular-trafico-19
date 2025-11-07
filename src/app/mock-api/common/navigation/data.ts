7f0d145a1580f1dde453e77b8681f26763957258
Eriesp15 <erikaec6r@gmail.com>
2025-11-07 02:51:55 -0400

    feat: update baggage navigation items and fix titles

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -6,21 +6,51 @@ export const defaultNavigation: ErpNavigationItem[] = [
         id   : 'baggage',
         title: 'Equipajes',
         type : 'collapsable',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:shopping-bag',
         children:[
             {
                 id   : 'follow',
                 title: 'Seguimiento',
                 type : 'basic',
-                icon : 'heroicons_outline:chart-pie',
+                icon : 'heroicons_outline:document-magnifying-glass',
                 link : '/baggage/follow'
             },
             {
                 id   : 'claim',
-                title: 'Relamos',
+                title: 'Reclamos',
+                type : 'collapsable',
+                icon : 'heroicons_outline:document',
+                link : '/baggage/claim/list',
+                children:[
+                    {
+                    id   : 'follow',
+                    title: 'Listado de reclamos',
+                    type : 'basic',
+                    icon : 'heroicons_outline:list-bullet',
+                    link : '/baggage/claim/list'
+                    },
+                    {
+                    id   : 'follow',
+                    title: 'Realizar reclamo',
+                    type : 'basic',
+                    icon : 'heroicons_outline:newspaper',
+                    link : '/baggage/claim/new'
+                    },
+                    {
+                    id   : 'follow',
+                    title: 'Visualizar reclamo',
+                    type : 'basic',
+                    icon : 'heroicons_outline:eye',
+                    link : '/baggage/follow'
+                    }
+                ]
+            },
+            {
+                id   : 'claim',
+                title: 'Indemnizaciones',
                 type : 'basic',
-                icon : 'heroicons_outline:chart-pie',
-                link : '/baggage/claim',
+                icon : 'heroicons_outline:banknotes',
+                link : '/baggage/compensation',
             }
         ]
     },
@@ -30,7 +60,7 @@ export const compactNavigation: ErpNavigationItem[] = [
         id   : 'baggage',
         title: 'Equipajes',
         type : 'collapsable',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:shopping-bag',
         children:[
             {
                 id   : 'follow',
@@ -41,10 +71,17 @@ export const compactNavigation: ErpNavigationItem[] = [
             },
             {
                 id   : 'claim',
-                title: 'Relamos',
+                title: 'Reclamos',
+                type : 'basic',
+                icon : 'heroicons_outline:document',
+                link : '/baggage/claim/list'
+            },
+            {
+                id   : 'claim',
+                title: 'Indemnizaciones',
                 type : 'basic',
                 icon : 'heroicons_outline:chart-pie',
-                link : '/baggage/claim'
+                link : '/baggage/compensation',
             }
         ]
     },
@@ -55,7 +92,7 @@ export const futuristicNavigation: ErpNavigationItem[] = [
         id   : 'baggage',
         title: 'Equipajes',
         type : 'collapsable',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:shopping-bag',
         children:[
             {
                 id   : 'follow',
@@ -66,10 +103,17 @@ export const futuristicNavigation: ErpNavigationItem[] = [
             },
             {
                 id   : 'claim',
-                title: 'Relamos',
+                title: 'Reclamos',
+                type : 'basic',
+                icon : 'heroicons_outline:document',
+                link : '/baggage/claim/list'
+            },
+            {
+                id   : 'claim',
+                title: 'Indemnizaciones',
                 type : 'basic',
                 icon : 'heroicons_outline:chart-pie',
-                link : '/baggage/claim'
+                link : '/baggage/compensation',
             }
         ]
     },
@@ -79,7 +123,7 @@ export const horizontalNavigation: ErpNavigationItem[] = [
         id   : 'baggage',
         title: 'Equipajes',
         type : 'collapsable',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:shopping-bag',
         children:[
             {
                 id   : 'follow',
@@ -90,10 +134,17 @@ export const horizontalNavigation: ErpNavigationItem[] = [
             },
             {
                 id   : 'claim',
-                title: 'Relamos',
+                title: 'Reclamos',
+                type : 'basic',
+                icon : 'heroicons_outline:document',
+                link : '/baggage/claim/list'
+            },
+            {
+                id   : 'claim',
+                title: 'Indemnizaciones',
                 type : 'basic',
                 icon : 'heroicons_outline:chart-pie',
-                link : '/baggage/claim'
+                link : '/baggage/compensation',
             }
         ]
     },
